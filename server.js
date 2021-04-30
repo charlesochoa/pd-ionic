426304fcfc0cb1d687d8cbf464308eb0a0cfeb2e
cochoa_hiberus <cochoa@hiberus.com>
2021-05-01 00:04:47 +0200

    dependencies changed

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const app = express();
-
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.use(express.static('www'));
+var express  = require('express');
+var app      = express();                               
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
 var cors = require('cors');
-                                 
+ 
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
 app.use(cors());
-
+ 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-  });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+ 
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
