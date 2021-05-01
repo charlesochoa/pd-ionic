c41b1303b1e064244c70c32a2770c36efcfb4265
cochoa_hiberus <cochoa@hiberus.com>
2021-05-01 15:17:31 +0200

    Revision server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express  = require('express');
 var app      = express();                               
-var morgan = require('morgan');            
+var morgan = require('morgan'); 
 var bodyParser = require('body-parser');    
 var cors = require('cors');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
 app.use(cors());
  
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '*');
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
  
